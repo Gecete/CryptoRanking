dfe2d2428f57387095c2ee13166a0104be3d6638
Gonzalo del Corral <gonzalo.del.corral@woodwing.com>
2022-06-03 17:09:54 +0200

    update endpoints

--- src/app/services/query.service.ts
+++ src/app/services/query.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from "rxjs";
 
 // This service is incharged of the API calls. 
@@ -8,14 +8,21 @@ import { Observable } from "rxjs";
 export class QueryService {
 
     private limit = "100";
-    private apiServer: string = "https://api.coinranking.com/v1/public/coins?limit=".concat(this.limit);
+    private apiServer: string = "https://api.coinranking.com/v2/coins?limit=".concat(this.limit);
+    private auth_token = 'coinranking4e5008f4513eeb3d3be9280a6bf6c83d4398fa39dff15344';
+    private headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.auth_token}`,
+        'Access-Control-Allow-Origin': '*'
+      })   
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
     
     //get 100 results starting from value 'offset' (for pagination)
     
     get(value:number): Observable<any> {
         let offset= "&offset="+String(value);
-        return this.http.get(this.apiServer.concat(offset));
+        return this.http.get(this.apiServer.concat(offset),{ headers: this.headers } );
     }
 }
